5c70e40822bcc7dc65560c90dae080fe467f428d
andres.martinez <andres.martinez@macroproyectos.com>
2023-06-07 11:50:04 -0500

    Se crea logica para codificar con SHA-256 y se implementa en LoginMB

--- src/main/java/com/unbosque/bean/LoginMB.java
+++ src/main/java/com/unbosque/bean/LoginMB.java
@@ -1,14 +1,22 @@
 package com.unbosque.bean;
 
+import java.security.NoSuchAlgorithmException;
+
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
+import org.jboss.logging.Logger;
+import org.primefaces.PrimeFaces;
+
 import com.unbosque.entity.Usuario;
 import com.unbosque.service.UsuarioService;
+import com.unbosque.utils.EncodeSHA256;
 
 @ManagedBean
 @SessionScoped
 public class LoginMB {
+	private final static Logger LOGGER = Logger.getLogger(LoginMB.class.getName());
 	private String usuario;
 	private String clave;
 	private int intentos;
@@ -16,18 +24,38 @@ public class LoginMB {
 	public void ingresar() {
 		UsuarioService usuarioService = new UsuarioService();
 		Usuario usuarioLogueado = usuarioService.getUsuario(usuario);
+		EncodeSHA256 encoder = new EncodeSHA256();
 
-		if (clave.equals(usuarioLogueado.getClave())) {
-			// TODO switch para redireccione dependiendo al usuario logueado
-			
-		} else {
-			this.intentos++;
-			// TODO mostrar mensaje de "Error al loguearse"
-			// TODO implementar componente de mensajes de PrimeFaces
+		try {
+			if (encoder.getStringEncoded(clave).equals(usuarioLogueado.getClave())) {
+				// TODO switch para redireccione dependiendo al usuario logueado
+				switch (usuarioLogueado.getTipoUsuario()) {
+				case "":
 
+					break;
+
+				default:
+					break;
+				}
+
+			} else {
+				this.intentos++;
+				showMessage("Credenciales incorrectas");
+				LOGGER.info("Credenciales incorrectas para el usuario " + this.usuario);
+				// TODO mostrar mensaje de "Error al loguearse"
+				// TODO implementar componente de mensajes de PrimeFaces
+
+			}
+		} catch (NoSuchAlgorithmException e) {
+			LOGGER.error("Error al intentar loguearse", e);
 		}
-		
-		System.out.println("Intentos de logueo: " + this.intentos);
+
+		LOGGER.info("Intentos de logueo: " + this.intentos);
+	}
+
+	public void showMessage(String msj) {
+		FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Mensaje", msj);
+		PrimeFaces.current().dialog().showMessageDynamic(message);
 	}
 
 	public String getUsuario() {
