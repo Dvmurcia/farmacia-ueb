752d77a7cbaf561cd7a2f7cbc3a9e371470e629b
andres.martinez <andres.martinez@macroproyectos.com>
2023-06-08 21:55:44 -0500

    Finalizo tabla y modal de agregar

--- src/main/java/com/unbosque/bean/ListarParametroMB.java
+++ src/main/java/com/unbosque/bean/ListarParametroMB.java
@@ -1,14 +1,16 @@
 package com.unbosque.bean;
 
 import java.util.List;
-
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
-
+import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 
 import org.jboss.logging.Logger;
+import org.primefaces.PrimeFaces;
+
 import com.unbosque.entity.Parametro;
 import com.unbosque.service.ParametroService;
 
@@ -17,7 +19,12 @@ import com.unbosque.service.ParametroService;
 public class ListarParametroMB {
 	private final static Logger LOGGER = Logger.getLogger(ListarParametroMB.class.getName());
 	private List<Parametro> parametros;
-	private Parametro parametroSeleccionado;
+
+	// Campos del formulario de creacion
+	private String nombreParametro;
+	private String descripcionParametro;
+	private String valorParametro;
+	private Integer estadoParametro;
 
 	@Inject
 	private ParametroService parametroService;
@@ -26,15 +33,48 @@ public class ListarParametroMB {
 	public void init() {
 		this.parametroService = new ParametroService();
 		this.parametros = this.parametroService.list();
-		LOGGER.info("Parametros cargados de la BD: " + this.parametros.size());
 	}
 
-	public Parametro getParametroSeleccionado() {
-		return parametroSeleccionado;
+	public void reset() {
+		PrimeFaces.current().resetInputs(":dialogNuevoParametro:parametroGrid");
 	}
 
-	public void setParametroSeleccionado(Parametro parametroSeleccionado) {
-		this.parametroSeleccionado = parametroSeleccionado;
+	public void crearParametro() {
+		Parametro nuevoParametro = new Parametro();
+
+		nuevoParametro.setNombreParametro(nombreParametro);
+		nuevoParametro.setDescripcionParametro(descripcionParametro);
+		nuevoParametro.setValorParametro(valorParametro);
+		nuevoParametro.setEstadoParametro(estadoParametro.byteValue());
+
+		this.parametroService.save(nuevoParametro);
+		System.out.println("Parametro insertado correctamente");
+		this.parametros = this.parametroService.list();
+
+		this.nombreParametro = null;
+		this.descripcionParametro = null;
+		this.valorParametro = null;
+		this.estadoParametro = null;
+		
+		PrimeFaces.current().executeScript("PF('nuevoParametro').hide();");
+		PrimeFaces.current().ajax().update(":form:dt-parametros");
+		// TODO Pendiente hacer registro de auditoria
+	}
+
+	public void addMessage(FacesMessage.Severity severity, String summary, String detail) {
+		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(severity, summary, detail));
+	}
+
+	public void showInfo() {
+		addMessage(FacesMessage.SEVERITY_INFO, "Informacion", "Operación exitosa");
+	}
+
+	public void showWarn() {
+		addMessage(FacesMessage.SEVERITY_WARN, "Advertencia", "Message Content");
+	}
+
+	public void showError() {
+		addMessage(FacesMessage.SEVERITY_ERROR, "Error", "Error al realizar la operación");
 	}
 
 	public List<Parametro> getParametros() {
@@ -45,4 +85,35 @@ public class ListarParametroMB {
 		this.parametroService = parametroService;
 	}
 
+	public String getNombreParametro() {
+		return nombreParametro;
+	}
+
+	public void setNombreParametro(String nombreParametro) {
+		this.nombreParametro = nombreParametro;
+	}
+
+	public String getDescripcionParametro() {
+		return descripcionParametro;
+	}
+
+	public void setDescripcionParametro(String descripcionParametro) {
+		this.descripcionParametro = descripcionParametro;
+	}
+
+	public String getValorParametro() {
+		return valorParametro;
+	}
+
+	public void setValorParametro(String valorParametro) {
+		this.valorParametro = valorParametro;
+	}
+
+	public Integer getEstadoParametro() {
+		return estadoParametro;
+	}
+
+	public void setEstadoParametro(Integer estadoParametro) {
+		this.estadoParametro = estadoParametro;
+	}
 }
